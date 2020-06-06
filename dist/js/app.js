16529dc579c9dee297153bafa521a7bb17cfedd8
ivanburda1986 <burda.ivan@gmail.com>
2020-06-07 00:20:01 +0200

    implemented a cross animated button for the mobile nav

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,23 +1,30 @@
 //UI Selectors
 const mobileNavigation = document.getElementById("main-nav-mobile-menu-options");
 const burgerToggler = document.getElementById("burger-toggler");
-const burger = document.getElementById("burger");
+const burgerLine1 = document.getElementById("burger-line1");
+const burgerLine2 = document.getElementById("burger-line2");
+const burgerLine3 = document.getElementById("burger-line3");
 
 //NAVIGATION - mobile
 burgerToggler.addEventListener("click", (e) => {
   if (e.srcElement.checked) {
     mobileNavigation.classList.add("anim-slideToScreen-endState");
-    burger.classList.add("burger-animate-endState");
+    burgerLine1.classList.add("line1-move", "line1-animate-endState");
+    burgerLine2.classList.add("line2-animate-endState");
+    burgerLine3.classList.add("line3-move", "line3-animate-endState");
 
   } else {
     mobileNavigation.classList.remove("anim-slideToScreen-endState");
-    burger.classList.remove("burger-animate-endState");
+    burgerLine1.classList.remove("line1-move", "line1-animate-endState");
+    burgerLine2.classList.remove("line2-animate-endState");
+    burgerLine3.classList.remove("line3-move", "line3-animate-endState");
+
   }
 })
 
 function closeMobileNavigation() {
   document.getElementById("main-nav-mobile-menu-options").classList.remove("anim-slideToScreen-endState");
-  document.getElementById("burger").classList.remove("burger-animate-endState");
+  //document.getElementById("burger").classList.remove("burger-animate-endState");
   document.getElementById("burger-toggler").checked = false;
 }
 
