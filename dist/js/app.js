a2ea9f0fce1b53833b1aafd0c8eb00161ee41bb5
ivanburda1986 <burda.ivan@gmail.com>
2020-06-06 18:40:55 +0200

    refactored also the blogposts

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,6 +1,3 @@
-// INIT
-const projectDetails = new ProjectDetailsUI();
-
 //UI Selectors
 const mobileNavigation = document.getElementById("main-nav-mobile-menu-options");
 const burgerToggler = document.getElementById("burger-toggler");
@@ -50,48 +47,6 @@ document.getElementById("nav-mobile-contact").addEventListener("click", (e) => {
   closeMobileNavigation();
 })
 
-
-// PORTFOLIO OVERLAYS
-const project1showBtn = document.getElementById('project1-showBtn');
-const project1hideBtn = document.getElementById('project1-hidebtn');
-const project2showBtn = document.getElementById('project2-showBtn');
-const project2hideBtn = document.getElementById('project2-hidebtn');
-const project3showBtn = document.getElementById('project3-showBtn');
-const project3hideBtn = document.getElementById('project3-hidebtn');
-const project4showBtn = document.getElementById('project4-showBtn');
-const project4hideBtn = document.getElementById('project4-hidebtn');
-
-project1showBtn.addEventListener('click', () => {
-  projectDetails.showProjectOverlay(1);
-});
-
-project1hideBtn.addEventListener('click', () => {
-  projectDetails.hideProjectOverlay(1);
-});
-
-project2showBtn.addEventListener('click', () => {
-  projectDetails.showProjectOverlay(2);
-});
-
-project2hideBtn.addEventListener('click', () => {
-  projectDetails.hideProjectOverlay(2);
-});
-
-project3showBtn.addEventListener('click', () => {
-  projectDetails.showProjectOverlay(3);
-});
-
-project3hideBtn.addEventListener('click', () => {
-  projectDetails.hideProjectOverlay(3);
-});
-
-project4showBtn.addEventListener('click', () => {
-  projectDetails.showProjectOverlay(4);
-});
-
-project4hideBtn.addEventListener('click', () => {
-  projectDetails.hideProjectOverlay(4);
-});
 
 
 
--- dist/js/project-overlay.js
+++ dist/js/project-overlay.js
@@ -18,3 +18,48 @@ class ProjectDetailsUI {
     console.log("goodbye");
   }
 }
+
+// INIT
+const projectDetails = new ProjectDetailsUI();
+
+// PORTFOLIO OVERLAYS
+const project1showBtn = document.getElementById('project1-showBtn');
+const project1hideBtn = document.getElementById('project1-hidebtn');
+const project2showBtn = document.getElementById('project2-showBtn');
+const project2hideBtn = document.getElementById('project2-hidebtn');
+const project3showBtn = document.getElementById('project3-showBtn');
+const project3hideBtn = document.getElementById('project3-hidebtn');
+const project4showBtn = document.getElementById('project4-showBtn');
+const project4hideBtn = document.getElementById('project4-hidebtn');
+
+project1showBtn.addEventListener('click', () => {
+  projectDetails.showProjectOverlay(1);
+});
+
+project1hideBtn.addEventListener('click', () => {
+  projectDetails.hideProjectOverlay(1);
+});
+
+project2showBtn.addEventListener('click', () => {
+  projectDetails.showProjectOverlay(2);
+});
+
+project2hideBtn.addEventListener('click', () => {
+  projectDetails.hideProjectOverlay(2);
+});
+
+project3showBtn.addEventListener('click', () => {
+  projectDetails.showProjectOverlay(3);
+});
+
+project3hideBtn.addEventListener('click', () => {
+  projectDetails.hideProjectOverlay(3);
+});
+
+project4showBtn.addEventListener('click', () => {
+  projectDetails.showProjectOverlay(4);
+});
+
+project4hideBtn.addEventListener('click', () => {
+  projectDetails.hideProjectOverlay(4);
+});
