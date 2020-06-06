3e50d2625e2df97e95ce5d2740658757221972ff
ivanburda1986 <burda.ivan@gmail.com>
2020-06-06 15:36:54 +0200

    implemented the overlay for projects

--- dist/js/project-overlay.js
+++ dist/js/project-overlay.js
@@ -1,15 +1,20 @@
 class ProjectDetailsUI {
   constructor() {
     this.project1Overlay = document.getElementById('project1-overlay-container');
+    this.project2Overlay = document.getElementById('project2-overlay-container');
+    this.project3Overlay = document.getElementById('project3-overlay-container');
+    this.project4Overlay = document.getElementById('project4-overlay-container');
   }
 
-  showProjectOverlay() {
-    this.project1Overlay.classList.add('show');
-    this.project1Overlay.classList.remove('hide');
+  showProjectOverlay(number) {
+    this[`project${number}Overlay`].classList.add('show');
+    this[`project${number}Overlay`].classList.remove('hide');
+    console.log("hello");
   }
 
-  hideProjectOverlay() {
-    this.project1Overlay.classList.remove('show');
-    this.project1Overlay.classList.add('hide');
+  hideProjectOverlay(number) {
+    this[`project${number}Overlay`].classList.remove('show');
+    this[`project${number}Overlay`].classList.add('hide');
+    console.log("goodbye");
   }
 }
